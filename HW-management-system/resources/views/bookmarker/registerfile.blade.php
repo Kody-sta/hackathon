c7eb11600f3453af26c515da073b25a0942f0a18
RyotaroKazama <Pontaro.1208@keio.jp>
2022-11-26 20:49:58 +0900

    入力画面のCSSを調整

--- HW-management-system/resources/views/bookmarker/registerfile.blade.php
+++ HW-management-system/resources/views/bookmarker/registerfile.blade.php
@@ -1,48 +1,45 @@
 <!DOCTYPE html>
 <html lang="ja">
- <head>
- <meta charset="utf-8">
- <title>入力画面</title>
- <meta name="description" content="">
- <link rel="stylesheet" href="registerfile.css">
-
- <script src="registerfile.js"></script>
- </head>
- 
- <body>
+<head>
+<meta charset="utf-8">
+<title>入力画面</title>
+<meta name="description" content="">
+<link rel="stylesheet" href="registerfile.css">
+<script src="registerfile.js"></script>
+</head>
+<body>
     <div class="contact">
-        <form action="">
+        <form method="post" action="resisterfile.blade.php">
             <table class="contact-table">
                 <tr>
-                    <th class="contact-item"><label class="label">日付:</label></th>
-                    <td class="contact-body">
+                    <th class="contact-header"><label class="label">日付:</label></th>
+                    <td class="contact-data">
+                        <input class="inputs" type="text" name="title">
+                    </td>
+                </tr>       
+                <tr>
+                    <th class="contact-header"><label class="label">タイトル:</label></th>
+                    <td class="contact-data">
                         <input class="inputs" type="text" name="title">
                     </td>
                 </tr>
-                
                 <tr>
-                    <th class="contact-item"><label class="label">タイトル:</label></th>
-                    <td class="contact-body">
-                        <input class="inputs" type="text" name="title">
-                    </td>
-                </tr>
-                <tr>
-                    <th class="contact-item"><label class="label">URL:</label></th>
-                    <td class="contact-body">
+                    <th class="contact-header"><label class="label">URL:</label></th>
+                    <td class="contact-data">
                         <input class="inputs" type="text" name="url">
                     </td>
                 </tr>
                 <tr>
-                    <th class="contact-item"><label class="label">メモ:</label></th>
-                    <td class="contact-body">
-                        <textarea class="form-textarea" name="memo">
+                    <th class="contact-header"><label class="label">メモ:</label></th>
+                    <td class="contact-data">
+                        <!-- <input class="inputs" type="text" name="memo"> -->
+                        <textarea class="form-textarea" name="memo"></textarea>
                     </td>
                 </tr>
             </table>
             <div class="btn-area">
-                <input type="button" value="登録する">
+                <input type="submit" value="登録する">
             </div>
-          
         </form>
     </div>
  </body>
