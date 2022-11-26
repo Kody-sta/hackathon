759d49cd06efefc9330d086e23bea8fb7207952b
Ika215 <summerwave345@gmail.com>
2022-11-26 19:30:22 +0900

    ルーティング

---
+++ HW-management-system/app/Http/Controllers/BookmarkerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Bookmarker;
+
+class BookmarkerController extends Controller
+{
+    public function index(Request $request)
+    {
+        $bookmarkers = Bookmarker::all();
+        return view('bookmarker.index', ['bookmarkers' => $bookmarkers]);
+    }
+}
