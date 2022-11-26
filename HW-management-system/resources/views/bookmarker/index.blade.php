ebe0f7427c08207c9ada06a71428071950843df2
kodai-nakajima <kou.nk519@gmail.com>
2022-11-26 18:46:21 +0900

    indexのページ

---
+++ HW-management-system/resources/views/bookmarker/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>BookMarker</title>
+        <meta name="description" content="BookMarkerで気になる記事やスクショを保存、管理！">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <!-- [if lt IE 9] -->
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+        <!-- [endif] -->
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <!----- header----->
+        <header></header>
+        <nav></nav>
+        <!----- /header ----->
+
+        <!----- main ----->
+        <div style="margin:0px;padding:0px;" align="center">
+          <table width="500px" style="border-collapse: collapse;border:1px solid #000000;background-color:#FFFFFF;color:#000000;text-align:left;">
+            <tbody>
+              <tr>
+                <th style="border:1px solid #000000;background-color:#808080;color:#FFFFFF;text-align:center;">
+                  日付&nbsp;
+                </th>
+                <th style="border:1px solid #000000;background-color:#808080;color:#FFFFFF;text-align:center;">
+                  タイトル&nbsp;
+                </th>
+                <th style="border:1px solid #000000;background-color:#808080;color:#FFFFFF;text-align:center;">
+                  URL&nbsp;
+                </th>
+                <th style="border:1px solid #000000;background-color:#808080;color:#FFFFFF;text-align:center;">
+                  メモ&nbsp;
+                </th>
+              </tr>
+              <tr>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;</td>
+              </tr>
+              <tr>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+              </tr>
+              <tr>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+                <td style="border:1px solid #000000;text-align:left;">&nbsp;
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <!----- /main ----->
+
+        <!----- footer ----->
+        <footer></footer>
+        <!----- /footer ----->
+    </body>
+</html>
